1eb0c4938db11ed1d6772fe956091d8ce362b344
Damien Leroy <dleroy@smad.be>
2025-03-28 11:35:47 +0100

    initial config for frontend pbl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,8 +10,8 @@ export const environment: Environment = {
 
   sentryDsn: 'https://6295834d69104f54b55cc0ebe4ada310@o1167067.ingest.sentry.io/6257761',
 
-  defaultActivityId: '4702',
-  defaultSkillId : '409159715167354071',
+  defaultActivityId: '1',
+  defaultSkillId : '2',
 
   allUsersGroupId: '3',
 
@@ -33,47 +33,8 @@ export const environment: Environment = {
   },
 
   redirects: { /* paths to be matched must not have a trailing slash */
-    'home': { id: '4702', path: [] },
-    'contents/4703': { id: '4702', path: [] },
+    'home': { id: '1', path: [] },
 
-    'snt': { id: '1067253748629066205', path: ['4702'] },
-    'contents/4707-4702-1067253748629066205': { id: '1067253748629066205', path: ['4702'] },
-
-    'snt-localisation': { id: '183305583351435935', path: ['4702','1067253748629066205'] },
-    'contents/4707-4702-1067253748629066205-183305583351435935': { id: '183305583351435935', path: ['4702','1067253748629066205'] },
-
-    'snt-donnees': { id: '653650670442840123', path: ['4702','1067253748629066205'] },
-    'contents/4707-4702-1067253748629066205-653650670442840123': { id: '653650670442840123', path: ['4702','1067253748629066205'] },
-
-    'snt-photo': { id: '1625996270397195025', path: ['4702','1067253748629066205'] },
-    'contents/4707-4702-1067253748629066205-1625996270397195025': { id: '1625996270397195025', path: ['4702','1067253748629066205'] },
-
-    'snt-internet': { id: '1107658684176222875', path: ['4702','1067253748629066205'] },
-    'contents/4707-4702-1067253748629066205-1107658684176222875': { id: '1107658684176222875', path: ['4702','1067253748629066205'] },
-
-    'motif-art': { id: '1625159049301502151', path: ['4702'] },
-    'contents/4707-4702-1625159049301502151': { id: '1625159049301502151', path: ['4702'] },
-
-    'algorea-serious-game': { id: '1471479157476024035', path: ['4702'] },
-    'contents/4707-4702-1471479157476024035': { id: '1471479157476024035', path: ['4702'] },
-
-    'algorea-adventure': { id: '100575556387408660', path: ['4702'] },
-    'contents/4707-4702-100575556387408660': { id: '100575556387408660', path: ['4702'] },
   }
 };
-
-type Preset = 'telecomParis';
-export const presets: Record<Preset, PartialDeep<Environment>> = {
-  telecomParis: {
-    theme: 'coursera-pt',
-    defaultTitle: 'Activities by Telecom Paris and Dartmouth college',
-  },
-};
-
-export function getPresetNameByOrigin(origin: string): Preset | null {
-  switch (origin) {
-    case 'https://telecom-paris.france-ioi.org': return 'telecomParis';
-    default: return null;
-  }
-}
 
