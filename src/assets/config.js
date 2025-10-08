844aa8578bd9f6995e22e2696c13dee47b3db3eb
Damien Leroy <dleroy@smad.be>
2025-10-08 09:52:37 +0200

    disable forum for the moment

--- src/assets/config.js
+++ src/assets/config.js
@@ -3,7 +3,6 @@ window.appConfig = {
   oauthServerUrl: 'https://login.france-ioi.org',
   oauthClientId: '56',
   searchApiUrl: 'https://74w4cx6w62uon4dtqinf6quqeu0qvunw.lambda-url.eu-west-3.on.aws',
-  forumServerUrl: 'wss://0j8i1y8ca2.execute-api.eu-west-3.amazonaws.com/fioi',
 
   sentryDsn: 'https://6295834d69104f54b55cc0ebe4ada310@o1167067.ingest.sentry.io/6257761',
 
