aab1d5f24f9af7bf1f31695287b04dd1fcea9f34
Damien Leroy <dleroy@smad.be>
2026-08-29 14:44:43 +0200

    use correct ws endpoint for beoi

--- src/assets/config.js
+++ src/assets/config.js
@@ -5,7 +5,7 @@ window.appConfig = {
 //  searchApiUrl: '',
 //  forumServerUrl: 'wss://cyduaoy46h.execute-api.eu-west-3.amazonaws.com/thy',
 
-  slsWsUrl: 'wss://7i695vtyx8.execute-api.eu-west-3.amazonaws.com/thy-prod',
+  slsWsUrl: 'wss://x6g6cbrgo0.execute-api.eu-central-1.amazonaws.com/beoi',
   slsApiUrl: '/sls',
 
   sentryDsn: 'https://6295834d69104f54b55cc0ebe4ada310@o1167067.ingest.sentry.io/6257761',
